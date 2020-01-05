e158852ab49d767ae4ed1ce6cf5308c9b9c9eb68
Sourav Agarwal <agarwalsourav005@gmail.com>
2020-01-05 10:52:58 +0530

    Adding Favouries Functionality

--- src/BankDetails.jsx
+++ src/BankDetails.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Table } from "react-bootstrap";
 import "./App.css";
 
-const getBankDetails = bankdetails => {
+const getBankDetails = (bankdetails, indexOfFirstBank, addBanktoFavourites) => {
+  let count = 0;
   const bankrows = bankdetails.map(bank => {
+    const bankIndex = indexOfFirstBank + count;
+    count += 1;
     return (
       <tr key={bank.ifsc}>
         <td>{bank.bank_name}</td>
@@ -13,6 +16,20 @@ const getBankDetails = bankdetails => {
         <td>{bank.city}</td>
         <td>{bank.district}</td>
         <td>{bank.state}</td>
+        <td>
+          {bank.favourites ? (
+            <button className="btn btn-outline-primary disabled">
+              Added to Favourites
+            </button>
+          ) : (
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => addBanktoFavourites(bankIndex)}
+            >
+              Add to Favourites
+            </button>
+          )}
+        </td>
       </tr>
     );
   });
@@ -20,7 +37,7 @@ const getBankDetails = bankdetails => {
 };
 
 function BankDetails(props) {
-  if (props.bankdetails.length) {
+  if (!props.loading && props.bankdetails.length) {
     return (
       <div className="m-4">
         <Table striped bordered hover size="sm">
@@ -33,9 +50,16 @@ function BankDetails(props) {
               <th>City</th>
               <th>District</th>
               <th>State</th>
+              <th></th>
             </tr>
           </thead>
-          <tbody>{getBankDetails(props.bankdetails)}</tbody>
+          <tbody>
+            {getBankDetails(
+              props.bankdetails,
+              props.indexOfFirstBank,
+              props.addBanktoFavourites
+            )}
+          </tbody>
         </Table>
       </div>
     );
--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -1,22 +1,42 @@
 import React from "react";
 
-export const Pagination = ({ banksPerPage, totalBanks, paginate }) => {
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalBanks / banksPerPage); i++) {
-    pageNumbers.push(i);
-  }
+export const Pagination = ({
+  banksPerPage,
+  totalBanks,
+  paginate,
+  currentSelectedPage,
+  loading
+}) => {
+  if (loading) return <h4>Loading...</h4>;
+  else {
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(totalBanks / banksPerPage); i++) {
+      pageNumbers.push(i);
+    }
 
-  return (
-    <nav className="m-5">
-      <ul className="pagination">
-        {pageNumbers.map(number => (
-          <li key={number} className="page-item">
-            <a onClick={() => paginate(number)} href="!#" className="page-link">
-              {number}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+    return (
+      <nav className="m-5">
+        <ul className="pagination">
+          {pageNumbers.map(number => (
+            <li
+              key={number}
+              className={
+                number === currentSelectedPage
+                  ? "page-item active"
+                  : "page-item"
+              }
+            >
+              <a
+                onClick={() => paginate(number)}
+                href="!#"
+                className="page-link"
+              >
+                {number}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  }
 };
